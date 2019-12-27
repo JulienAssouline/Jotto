01a445014be996b6dd180c012a9780acd24d3e43
JulienAssouline <julien1993@hotmail.ca>
2019-12-27 17:41:02 -0500

    create getLetterMatchCount helper function

---
+++ client/src/helpers/index.test.js
@@ -0,0 +1,20 @@
+import { getLetterMatchCount } from "./";
+
+import { shallow } from "enzyme";
+
+describe("getLetterMatchCount", () => {
+  const secretWord = "party";
+
+  test("returns correct count when there a no matching letters", () => {
+    const letterMatchCount = getLetterMatchCount("bones", secretWord);
+    expect(letterMatchCount).toBe(0);
+  });
+  test("returns the correct count where there are 3 matching letters", () => {
+    const letterMatchCount = getLetterMatchCount("train", secretWord);
+    expect(letterMatchCount).toBe(3);
+  });
+  test("returns correct count when there are duplicate letters in guess", () => {
+    const letterMatchCount = getLetterMatchCount("parka", secretWord);
+    expect(letterMatchCount).toBe(3);
+  });
+});
